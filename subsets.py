f7facfc55d0081436ab11f6c27856f6a7e424670
gobrin111 <143447782+gobrin111@users.noreply.github.com>
2025-09-01 17:33:23 -0400

    added comments to describe code as per project requirement

--- subsets.py
+++ subsets.py
@@ -1,8 +1,11 @@
 import pandas as pd
 df = pd.read_excel("Data.xlsx")
+# df has original data
 
 df_25 = df.sample(frac=0.25, random_state=42)
 df_75 = df.drop(df_25.index)
+# split data into the respective subsets for training and testing
 
 df_25.to_csv("test.csv", index=False)
 df_75.to_csv("training.csv", index=False)
+# save the data to a csv file for later use
