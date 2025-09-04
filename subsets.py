46469d84ad3584bd69bf54b814b1da5d12f77039
gobrin111 <143447782+gobrin111@users.noreply.github.com>
2025-09-04 13:53:32 -0400

    applied the probability distribution method and finished the training and added the accuracy measurement

--- subsets.py
+++ subsets.py
@@ -1,11 +1,66 @@
 import pandas as pd
-df = pd.read_excel("Data.xlsx")
-# df has original data
+import math
 
-df_25 = df.sample(frac=0.25, random_state=42)
-df_75 = df.drop(df_25.index)
-# split data into the respective subsets for training and testing
 
-df_25.to_csv("test.csv", index=False)
-df_75.to_csv("training.csv", index=False)
-# save the data to a csv file for later use
+class Classifier:
+    def __init__(self):
+        self.benign_variance = None
+        self.malignant_variance = None
+        self.benign_mean = None
+        self.malignant_mean = None
+        self.df_75_zeros = None
+        self.df_75_ones = None
+        self.df_75 = None
+        self.df_25 = None
+        self.df = None
+        self.accuracy = None
+
+    def train(self, file):
+        self.df = pd.read_excel(file)
+        # df has original data
+
+        # split data into the respective subsets for training and testing
+        self.df_25 = self.df.sample(frac=0.25, random_state=42) # so the split is same throughout iterations, so the accuracy is the same
+        self.df_75 = self.df.drop(self.df_25.index)
+
+        # sort training data into two with the malignant and the benign
+        self.df_75_ones = self.df_75[self.df_75.iloc[:, 30] == 1]
+        self.df_75_zeros = self.df_75[self.df_75.iloc[:, 30] == 0]
+
+        # uses panda function to get the mean of each column except for the last one
+        self.malignant_mean = self.df_75_zeros.iloc[:, :30].mean().values
+        self.benign_mean = self.df_75_ones.iloc[:, :30].mean().values
+
+        # uses panda function to get the variance of each column except for the last one
+        self.malignant_variance = self.df_75_zeros.iloc[:, :30].var().values
+        self.benign_variance = self.df_75_ones.iloc[:, :30].var().values
+
+    def testRow(self, currentRow):
+        # holders for the value of each class
+        malignantVal = 1
+        benignVal = 1
+        for i in range(30):
+            # calculate using probability distribution
+            malignantVal *= (1 / ((math.sqrt(2 * math.pi)) * math.sqrt(self.malignant_variance[i]))) * (math.e ** (-(((currentRow[i] - self.malignant_mean[i]) ** 2) / (2 * self.malignant_variance[i]))))
+            benignVal *= (1 / ((math.sqrt(2 * math.pi)) * math.sqrt(self.benign_variance[i]))) * (math.e ** (-(((currentRow[i] - self.benign_mean[i]) ** 2) / (2 * self.benign_variance[i]))))
+
+        # then compare the two values and select the result
+        if malignantVal > benignVal:
+            return 0
+        else:
+            return 1
+
+    def runTest(self):
+        # measure accuracy with the actual answer
+        self.accuracy = 0
+        for i, row in self.df_25.iterrows():
+            result = self.testRow(row)
+            if result == row[30]:
+                self.accuracy += 1
+        self.accuracy /= len(self.df_25)
+        print("Accuracy: ", self.accuracy)
+
+
+gnbc = Classifier()
+gnbc.train("Data.xlsx")
+gnbc.runTest()
